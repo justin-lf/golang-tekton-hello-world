c1f7de60c0d94ace9d93d06ec071eaaea2a503e6
justin.lowry <justin@leapfrog.ai>
2021-05-09 14:02:08 -1000

    Update

--- hello_server.go
+++ hello_server.go
@@ -61,7 +61,9 @@ func waitForShutdown(srv *http.Server) {
 	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	srv.Shutdown(ctx)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("Shutting down")
 	os.Exit(0)
